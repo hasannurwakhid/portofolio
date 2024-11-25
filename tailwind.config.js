75f7de8e1aa8301ab246d7593613a3a837ae77ee
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-11-25 10:13:31 +0700

    add tech stack section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,19 @@ module.exports = {
       screens: {
         "2xl": "1320px",
       },
+      animation: {
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": {
+            transform: "rotate(-90deg)",
+          },
+          "50%": {
+            transform: "rotate(90deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
